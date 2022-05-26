c566d8daaba69bc10d7829aacf6b23a942a7b823
PureFunctor <purefunctor@gmail.com>
2022-05-26 18:25:57 +0800

    In the beginning, there was darkness

---
+++ lily-ast/src/type.rs
@@ -0,0 +1,135 @@
+//! Defines the type of types in `lily` along with associated operations.
+use derivative::Derivative;
+use std::rc::Rc;
+
+/// The type of types in `lily`.
+#[derive(Debug, Derivative)]
+#[derivative(PartialEq, Eq)]
+pub enum Type<Ann> {
+    /// Universal quantification.
+    Forall {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        name: String,
+        kind: Option<Rc<Type<Ann>>>,
+        r#type: Rc<Type<Ann>>,
+    },
+    /// Skolem type variables.
+    ///
+    /// This type is usually synthesized by the compiler through skolemization.
+    Skolem {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        name: String,
+    },
+    /// Unification variables.
+    ///
+    /// This type is usually synthesized by the compiler through unification.
+    Unsolved {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        name: String,
+    },
+    /// Syntactic type variables.
+    Variable {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        name: String,
+    },
+    /// Type constructors.
+    Constructor {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        name: String,
+    },
+    /// Type application.
+    Application {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        function: Rc<Type<Ann>>,
+        argument: Rc<Type<Ann>>,
+    },
+    /// Kind application.
+    ///
+    /// This type is usually synthesized by the compiler in the kind checker.
+    KindApplication {
+        #[derivative(PartialEq = "ignore")]
+        ann: Ann,
+        function: Rc<Type<Ann>>,
+        argument: Rc<Type<Ann>>,
+    },
+}
+
+impl<Ann> Type<Ann> {
+    /// Returns a reference to a type's annotation.
+    pub fn ann(&self) -> &Ann {
+        match self {
+            Type::Forall {
+                ann,
+                name: _,
+                kind: _,
+                r#type: _,
+            } => ann,
+            Type::Skolem { ann, name: _ } => ann,
+            Type::Unsolved { ann, name: _ } => ann,
+            Type::Variable { ann, name: _ } => ann,
+            Type::Constructor { ann, name: _ } => ann,
+            Type::Application {
+                ann,
+                function: _,
+                argument: _,
+            } => ann,
+            Type::KindApplication {
+                ann,
+                function: _,
+                argument: _,
+            } => ann,
+        }
+    }
+
+    /// Returns `true` if the type is [`Forall`].
+    ///
+    /// [`Forall`]: Type::Forall
+    #[must_use]
+    pub fn is_forall(&self) -> bool {
+        matches!(self, Self::Forall { .. })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ann_is_ignored_in_eq() {
+        let x = Type::Forall {
+            ann: 0,
+            name: "a".into(),
+            kind: Rc::new(Type::Constructor {
+                ann: 0,
+                name: "Type".into(),
+            })
+            .into(),
+            r#type: Rc::new(Type::Variable {
+                ann: 0,
+                name: "a".into(),
+            }),
+        };
+
+        let y = Type::Forall {
+            ann: 1,
+            name: "a".into(),
+            kind: Rc::new(Type::Constructor {
+                ann: 2,
+                name: "Type".into(),
+            })
+            .into(),
+            r#type: Rc::new(Type::Variable {
+                ann: 3,
+                name: "a".into(),
+            }),
+        };
+
+        assert_eq!(x, y);
+    }
+}
