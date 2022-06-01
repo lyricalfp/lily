b25894d22a22d8d66179dcf7ed511118a3ef6491
PureFunctor <purefunctor@gmail.com>
2022-06-01 12:04:04 +0800

    Implement initial grammar

--- lily-ast/src/lib.rs
+++ lily-ast/src/lib.rs
@@ -1,3 +1,9 @@
+#[macro_use]
+extern crate lalrpop_util;
+
+lalrpop_mod!(pub grammar);
+
 pub mod ann;
 pub mod colosseum;
+pub mod parser;
 pub mod types;
---
+++ lily-ast/src/parser.rs
@@ -0,0 +1,12 @@
+#[test]
+fn it_works_as_intended() {
+    use crate::{colosseum::Colosseum, grammar};
+
+    let arena = bumpalo::Bump::new();
+    let mut colosseum = Colosseum::new(&arena);
+
+    let input = "(a :: Type)";
+    let parsed = grammar::Type0Parser::new().parse(&mut colosseum, input);
+
+    println!("{:?}", parsed);
+}
