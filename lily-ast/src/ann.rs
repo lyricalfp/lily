167d5f0cb01aebaf7734b9dc79a4d903050f67de
PureFunctor <purefunctor@gmail.com>
2022-06-01 09:59:27 +0800

    Make source annotations use byte offsets

--- lily-ast/src/ann.rs
+++ lily-ast/src/ann.rs
@@ -1,16 +1,12 @@
 //! This module defines the annotation used for AST nodes.
 
-/// A pair of a line and a column.
+/// A pair of starting and ending byte offsets.
 #[derive(Debug, PartialEq, Eq, Hash)]
-pub struct Span(u32, u32);
-
-/// A pair of a starting and ending span.
-#[derive(Debug, PartialEq, Eq, Hash)]
-pub struct Range(Span, Span);
+pub struct Span(pub u32, pub u32);
 
 /// The provenance of an AST node.
 #[derive(Debug, PartialEq, Eq, Hash)]
 pub enum Ann {
     FromCompiler,
-    FromSource(Range),
+    FromSource(Span),  // TODO: add module name here
 }
