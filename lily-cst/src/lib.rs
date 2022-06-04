8281acc6885f4d3c67c673160de57d27d08e688a
PureFunctor <purefunctor@gmail.com>
2022-06-04 13:10:12 +0800

    Implement basic lexer

---
+++ lily-cst/src/lib.rs
@@ -0,0 +1,135 @@
+use fancy_regex::Regex;
+
+#[derive(Debug)]
+pub enum Error {
+    UnrecognizedToken(usize),
+}
+
+#[derive(Debug)]
+pub enum Token<'a> {
+    DigitDouble(f64),
+    DigitInteger(i64),
+    NameLower(&'a str),
+    NameProper(&'a str),
+    NameSymbol(&'a str),
+    ParenLeft,
+    ParenRight,
+    BracketLeft,
+    BracketRight,
+    SquareLeft,
+    SquareRight,
+    SymbolComma,
+}
+
+type Pattern<'a> = &'a str;
+type Creator<'a> = &'a dyn Fn(&'a str) -> Result<Token<'a>, Error>;
+
+#[derive(Default)]
+struct Builder<'a>(Vec<(Pattern<'a>, Creator<'a>)>);
+
+impl<'a> Builder<'a> {
+    fn push(mut self, p: Pattern<'a>, c: Creator<'a>) -> Self {
+        self.0.push((p, c));
+        self
+    }
+
+    fn build(self) -> Vec<(Regex, Creator<'a>)> {
+        self.0
+            .into_iter()
+            .map(|(pattern, creator)| {
+                (
+                    Regex::new(format!("^{}", pattern).as_str()).unwrap(),
+                    creator,
+                )
+            })
+            .collect()
+    }
+}
+
+pub struct Lexer<'a> {
+    offset: usize,
+    source: &'a str,
+    patterns: Vec<(Regex, Creator<'a>)>,
+}
+
+impl<'a> Lexer<'a> {
+    pub fn new(source: &'a str) -> Self {
+        let offset = 0;
+        let patterns = Builder::default()
+            .push(r"[\p{Lu}_][\p{L}+_0-9']*", &|i| Ok(Token::NameProper(i)))
+            .push(r"[\p{Ll}_][\p{L}+_0-9']*", &|i| Ok(Token::NameLower(i)))
+            .push(r"([:!#$%&*+./<=>?@\\^|~-]|(?!\p{P})\p{S})+", &|i| {
+                Ok(Token::NameSymbol(i))
+            })
+            .push(r"[\[\](){},]", &|i| {
+                Ok(match i {
+                    "(" => Token::ParenLeft,
+                    ")" => Token::ParenRight,
+                    "[" => Token::SquareLeft,
+                    "]" => Token::SquareRight,
+                    "{" => Token::BracketLeft,
+                    "}" => Token::BracketRight,
+                    "," => Token::SymbolComma,
+                    _ => panic!("Lexer::new - this path is never taken"),
+                })
+            })
+            .build();
+        Self {
+            offset,
+            source,
+            patterns,
+        }
+    }
+
+    #[inline]
+    fn window(&self) -> &'a str {
+        &self.source[self.offset..]
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Result<(usize, Token<'a>, usize), Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // handle eof
+        if self.offset >= self.source.len() {
+            return None;
+        }
+        // skip whitespaces
+        let whitespace = Regex::new(r"^\s+").unwrap();
+        if let Ok(Some(m)) = whitespace.find(self.window()) {
+            self.offset += m.end();
+        }
+        // everything else
+        let longest_match = self
+            .patterns
+            .iter()
+            .filter_map(|(regex, creator)| {
+                if let Ok(Some(m)) = regex.find(self.window()) {
+                    Some((m.end(), creator(m.as_str())))
+                } else {
+                    None
+                }
+            })
+            .max_by_key(|(length, _)| *length);
+
+        match longest_match {
+            Some((length, created)) => match created {
+                Ok(token) => {
+                    let left_offset = self.offset;
+                    self.offset += length;
+                    Some(Ok((left_offset, token, self.offset)))
+                }
+                Err(error) => Some(Err(error)),
+            },
+            None => Some(Err(Error::UnrecognizedToken(self.offset))),
+        }
+    }
+}
+
+#[test]
+pub fn it_works_as_intended() {
+    for token in Lexer::new("[(hello # world), a0 + b1, _a + _b_ * _c']").take(20) {
+        println!("{:?}", token);
+    }
+}
