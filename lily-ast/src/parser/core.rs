48d4beb0f66e67329b25aef3890c35fcc9194b1a
Justin Garcia <functor@duck.com>
2022-08-30 15:16:43 +0800

    Implement basic parsing for expressions

--- lily-ast/src/parser/core.rs
+++ lily-ast/src/parser/core.rs
@@ -4,12 +4,14 @@ use smol_str::SmolStr;
 
 use crate::{
     expect,
-    lexer::types::{DelimiterK, DigitK, IdentifierK, OperatorK, Token, TokenK},
+    lexer::types::{DelimiterK, DigitK, IdentifierK, LayoutK, OperatorK, Token, TokenK},
     parser::{
         cursor::Cursor,
         errors::ParseError,
         fixity::{Associativity, Fixity, FixityMap},
-        types::{GreaterPattern, GreaterPatternK, LesserPattern, LesserPatternK},
+        types::{
+            Expression, ExpressionK, GreaterPattern, GreaterPatternK, LesserPattern, LesserPatternK,
+        },
     },
 };
 
@@ -247,6 +249,129 @@ where
         }
 
         Ok(greater_patterns)
+    }
+
+    pub fn expression_atom(&mut self, fixity_map: &FixityMap) -> anyhow::Result<Expression> {
+        let Token {
+            begin, end, kind, ..
+        } = self.take()?;
+
+        if let TokenK::Digit(DigitK::Int) = kind {
+            return Ok(Expression {
+                begin,
+                end,
+                kind: ExpressionK::Integer(SmolStr::new(&self.source[begin..end])),
+            });
+        }
+
+        if let TokenK::Digit(DigitK::Float) = kind {
+            return Ok(Expression {
+                begin,
+                end,
+                kind: ExpressionK::Float(SmolStr::new(&self.source[begin..end])),
+            });
+        }
+
+        if let TokenK::Identifier(IdentifierK::Lower) = kind {
+            return Ok(Expression {
+                begin,
+                end,
+                kind: ExpressionK::Variable(SmolStr::new(&self.source[begin..end])),
+            });
+        }
+
+        if let TokenK::Identifier(IdentifierK::Upper) = kind {
+            return Ok(Expression {
+                begin,
+                end,
+                kind: ExpressionK::Constructor(SmolStr::new(&self.source[begin..end])),
+            });
+        }
+
+        if let TokenK::OpenDelimiter(DelimiterK::Round) = kind {
+            let expression = self.expression_zero(fixity_map)?;
+            let Token { end, .. } = expect!(self, TokenK::CloseDelimiter(DelimiterK::Round));
+            return Ok(Expression {
+                begin,
+                end,
+                kind: ExpressionK::Parenthesized(Box::new(expression)),
+            });
+        }
+
+        bail!(ParseError::UnexpectedToken(kind));
+    }
+
+    pub fn expression_core(
+        &mut self,
+        fixity_map: &FixityMap,
+        minimum_power: u8,
+    ) -> anyhow::Result<Expression> {
+        let mut accumulator = self.expression_atom(fixity_map)?;
+
+        loop {
+            if let TokenK::Layout(LayoutK::Separator) | TokenK::CloseDelimiter(DelimiterK::Round) =
+                self.peek()?.kind
+            {
+                break;
+            }
+
+            if let Token {
+                begin,
+                end,
+                kind: TokenK::Operator(OperatorK::Source),
+                ..
+            } = self.peek()?
+            {
+                let source_range = *begin..*end;
+                let operator = SmolStr::new(&self.source[source_range]);
+
+                let (left_power, right_power) = fixity_map
+                    .get(&operator)
+                    .context(ParseError::InternalError(
+                        "Unknown operator binding power!".to_string(),
+                    ))?
+                    .as_pair();
+
+                if left_power < minimum_power {
+                    break;
+                } else {
+                    self.take()?;
+                }
+
+                let argument = self.expression_core(fixity_map, right_power)?;
+                accumulator = Expression {
+                    begin: accumulator.begin,
+                    end: argument.end,
+                    kind: ExpressionK::BinaryOperator(
+                        Box::new(accumulator),
+                        operator,
+                        Box::new(argument),
+                    ),
+                };
+                continue;
+            }
+
+            let argument = self.expression_atom(fixity_map)?;
+            match &mut accumulator.kind {
+                ExpressionK::Application(spines) => {
+                    accumulator.end = argument.end;
+                    spines.push(argument);
+                }
+                _ => {
+                    accumulator = Expression {
+                        begin: accumulator.begin,
+                        end: argument.end,
+                        kind: ExpressionK::Application(vec![accumulator, argument]),
+                    }
+                }
+            }
+        }
+
+        Ok(accumulator)
+    }
+
+    pub fn expression_zero(&mut self, fixity_map: &FixityMap) -> anyhow::Result<Expression> {
+        self.expression_core(fixity_map, 0)
     }
 
     pub fn declaration(&mut self, _: &FixityMap) -> anyhow::Result<()> {
