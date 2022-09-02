529a2123940d98bfcd5e22542ccc8a16da64cfb6
Justin Garcia <functor@duck.com>
2022-09-02 12:43:04 +0800

    Add test for a more complex expression

--- lily-parser/tests/parser_tests.rs
+++ lily-parser/tests/parser_tests.rs
@@ -19,3 +19,15 @@ example = a + b + c
 
     insta::assert_debug_snapshot!(parse_top_level(source));
 }
+
+#[test]
+pub fn top_level_2() {
+    let source = r"
+infixl 1 add as +
+infixl 2 add as *
+
+example = a + b * (c + d) + e
+";
+
+    insta::assert_debug_snapshot!(parse_top_level(source));
+}
